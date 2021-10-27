f50b353322337ae78be680d80e0e731bf37dce1d
Francis Agulto <francis.agulto@wpengine.com>
2021-10-27 12:29:18 -0500

    upated faust/next version

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import "faust.config";
-import { HeadlessProvider } from "@faustjs/next";
+import { FaustProvider } from "@faustjs/next";
 import { client } from "client";
 import type { AppProps } from "next/app";
 import "../../styles/global.css";
+import React from "react";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <HeadlessProvider client={client} pageProps={pageProps}>
+      <FaustProvider client={client} pageProps={pageProps}>
         <Component {...pageProps} />
-      </HeadlessProvider>
+      </FaustProvider>
     </>
   );
 }
