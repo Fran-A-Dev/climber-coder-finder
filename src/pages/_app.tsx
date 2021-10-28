3aa93a94207a15b02f995d77f7f209fd08dd71e8
Blake Wilson <blake@blake.id>
2021-10-28 11:01:50 -0500

    fix: resolve breaking changes in Faust

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "faust.config";
-import { HeadlessProvider } from "@faustjs/next";
+import { FaustProvider } from "@faustjs/next";
 import { client } from "client";
 import type { AppProps } from "next/app";
 import "../../styles/global.css";
@@ -8,9 +8,9 @@ import React from "react";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <HeadlessProvider client={client} pageProps={pageProps}>
+      <FaustProvider client={client} pageProps={pageProps}>
         <Component {...pageProps} />
-      </HeadlessProvider>
+      </FaustProvider>
     </>
   );
 }
